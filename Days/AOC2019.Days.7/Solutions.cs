6719ec6219560d7e164f57a0e38a8f4509a63f7c
Rian Katoen <rian.katoen@aaa-riskfinance.nl>
2019-12-25 17:55:21 +0100

    Day 7 part one.

--- Days/AOC2019.Days.7/Solutions.cs
+++ Days/AOC2019.Days.7/Solutions.cs
@@ -1,5 +1,7 @@
-using AOC2019.Modules.Intcode;
+using AOC2019.Modules.Amplification;
 using AOC2019.Modules.Utilities;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -14,17 +16,49 @@ namespace AOC2019.Days._7
         }
 
 
-        private IntcodeProgram Program() => new IntcodeProgram(EmbeddedResources.Read(GetType(), "input.txt"));
-
+        private string Memory() => EmbeddedResources.Read(GetType(), "input.txt");
 
         [Fact(DisplayName = "Part One")]
         public void PartOne()
         {
-            var program = Program();
-            program.Input = 1;
-            program.Execute();
+            var amplificationCircuit = new AmplificationCircuit(Memory());
+            var result = new Dictionary<string, int>();
+            var numbers = new int[] { 0, 1, 2, 3, 4 };
+            foreach(var i in numbers)
+            {
+                var jNumbers = numbers.Where(no => no != i).ToArray();
+                foreach (var j in jNumbers)
+                {
+                    var kNumbers = jNumbers.Where(no => no != j).ToArray();
+                    foreach (var k in kNumbers)
+                    {
+                        var nNumbers = kNumbers.Where(no => no != k).ToArray();
+                        foreach (var n in nNumbers)
+                        {
+                            var mNumbers = nNumbers.Where(no => no != n).ToArray();
+                            foreach (var m in mNumbers)
+                            {
+                                var settings = $"{i},{j},{k},{n},{m}";
+                                amplificationCircuit.SetPhaseSettings(settings);
+                                amplificationCircuit.Execute();
 
-            _output.WriteLine("WUT");
+                                result.Add(settings, amplificationCircuit.Signal);
+                            }
+                        }
+                    }
+                }
+            }
+
+            Assert.Equal(120, result.Count);
+
+            var maxSignal = result.Max(kv => kv.Value);
+            var maxResults = result.Where(kv => kv.Value == maxSignal);
+
+            _output.WriteLine($"No. of max results: {maxResults.Count()}");
+            foreach (var maxResult in maxResults)
+            {
+                _output.WriteLine($"{maxResult.Key}: {maxResult.Value}");
+            }
         }
     }
 }
